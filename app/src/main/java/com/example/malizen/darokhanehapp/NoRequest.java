2b348c39b9e2573c5380182861e096dd526f7424
Farzaneh <fbokharaei1993@gmail.com>
2019-01-16 17:18:12 +0330

    Add NO_Request Activity

---
+++ app/src/main/java/com/example/malizen/darokhanehapp/NoRequest.java
@@ -0,0 +1,13 @@
+package com.example.malizen.darokhanehapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_request);
+    }
+}
