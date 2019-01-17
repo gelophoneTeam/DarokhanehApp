065d3e8b4dc5e17d2e652c4524610b98fa3437be
Leila <leilamuhammadi1079@gmail.com>
2019-01-17 13:25:02 +0330

    Add verification code page

---
+++ app/src/main/java/com/example/malizen/darokhanehapp/VerificationCode.java
@@ -0,0 +1,13 @@
+package com.example.malizen.darokhanehapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerificationCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification_code);
+    }
+}
