d5b64b9ebbdb8eac1b774acfe244d95fcff61f03
Farzaneh <fbokharaei1993@gmail.com>
2019-02-02 16:07:47 +0330

    Add flowingdawer to Request Activity

--- app/src/main/java/com/example/malizen/darokhanehapp/Request.java
+++ app/src/main/java/com/example/malizen/darokhanehapp/Request.java
@@ -1,10 +1,22 @@
 package com.example.malizen.darokhanehapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
 
 public class Request extends AppCompatActivity {
+    private FlowingDrawer flowingDrawer;
+
     public static int[] osImages = {
             R.drawable.ic_group_784,
             R.drawable.ic_group_787,
@@ -38,7 +50,50 @@ public class Request extends AppCompatActivity {
         setContentView(R.layout.activity_request);
 
         listview = (ListView) findViewById(R.id.listview);
-        listview.setAdapter(new Adaptor(this, osNameList, osImages,osnumber,osBookMark));
+        listview.setAdapter(new Adaptor(this, osNameList, osImages,osnumber,osBookMark));  flowingDrawer = (FlowingDrawer) findViewById(R.id.flowingdrawer);
+        flowingDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
 
+        setupTolbar();
+
+        //remove appname from toolbar
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        //navigation
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Toast.makeText(Request.this,item.getTitle(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+    }
+
+    private void setupTolbar() {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar.setLayoutDirection(Gravity.END);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flowingDrawer.toggleMenu();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (flowingDrawer.isMenuVisible()) {
+            flowingDrawer.closeMenu();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
